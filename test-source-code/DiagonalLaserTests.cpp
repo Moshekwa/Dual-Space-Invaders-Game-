aa42838b489b6c842617e4f1438ed47545b3f518
Moshekwa Matthews Malatji <1387556@students.wits.ac.za>
2019-09-23 20:20:30 +0200

    Upward diagonal laser movement test successful

--- test-source-code/DiagonalLaserTests.cpp
+++ test-source-code/DiagonalLaserTests.cpp
@@ -23,10 +23,31 @@ TEST_CASE("Checking if Diagonal Laser moves with the canon")
 	
     auto _laserCanon = LaserCanon(xPosition, yPosition, 1, 3);
     auto _diagonalLaser = DiagonalLaser(_laserCanon, 1);
+	
     auto [x_position, y_position] = _diagonalLaser.entityPosition();
     CHECK(x_position == xPosition);
     _diagonalLaser.moveDiagonalLaserWithCanon(_laserCanon);
 	
 	auto [x_position2, y_position2] = _diagonalLaser.entityPosition();
     CHECK(xPosition2 == x_position2);
+}
+
+TEST_CASE("Testing Diagonal Laser move functionality")
+{
+	auto xPosition = 190;
+    auto yPosition = 480;
+	auto entitySpeed = 5;
+	auto xPosition2 = xPosition + entitySpeed;
+    auto yPosition2 = yPosition - entitySpeed;
+    
+	
+	auto _laserCanon = LaserCanon(xPosition, yPosition, 1, 3);
+    auto _diagonalLaser = DiagonalLaser(_laserCanon, 1);
+	
+    
+	
+	_diagonalLaser.move(Direction::UP);
+	auto [x_position, y_position] = _diagonalLaser.entityPosition();
+	CHECK(x_position == xPosition2);
+	CHECK(y_position == yPosition2);
 }
