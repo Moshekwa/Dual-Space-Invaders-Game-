9337b6c6e9c98215b9062810c5c14536e6a92849
Moshekwa Matthews Malatji <1387556@students.wits.ac.za>
2019-09-23 20:25:30 +0200

    Downwards diagonal laser movement test successful

--- test-source-code/DiagonalLaserTests.cpp
+++ test-source-code/DiagonalLaserTests.cpp
@@ -19,35 +19,48 @@ TEST_CASE("Checking if Diagonal Laser moves with the canon")
     auto xPosition = 190;
     auto yPosition = 480;
     auto entitySpeed = 6;
-	auto xPosition2 = xPosition + entitySpeed ;
-	
+    auto xPosition2 = xPosition + entitySpeed;
+
     auto _laserCanon = LaserCanon(xPosition, yPosition, 1, 3);
     auto _diagonalLaser = DiagonalLaser(_laserCanon, 1);
-	
+
     auto [x_position, y_position] = _diagonalLaser.entityPosition();
     CHECK(x_position == xPosition);
     _diagonalLaser.moveDiagonalLaserWithCanon(_laserCanon);
-	
-	auto [x_position2, y_position2] = _diagonalLaser.entityPosition();
+
+    auto [x_position2, y_position2] = _diagonalLaser.entityPosition();
     CHECK(xPosition2 == x_position2);
 }
 
-TEST_CASE("Testing Diagonal Laser move functionality")
+TEST_CASE("Testing Diagonal Laser upward movement functionality")
 {
-	auto xPosition = 190;
+    auto xPosition = 190;
     auto yPosition = 480;
-	auto entitySpeed = 5;
-	auto xPosition2 = xPosition + entitySpeed;
-    auto yPosition2 = yPosition - entitySpeed;
-    
-	
+    auto entitySpeed = 5;
+    auto xPosition_upward = xPosition + entitySpeed;
+    auto yPosition_upward = yPosition - entitySpeed;
+
+    auto _laserCanon = LaserCanon(xPosition, yPosition, 1, 3);
+    auto _diagonalLaser = DiagonalLaser(_laserCanon, 1);
+
+    _diagonalLaser.move(Direction::UP);
+    auto [x_position, y_position] = _diagonalLaser.entityPosition();
+    CHECK(x_position == xPosition_upward);
+    CHECK(y_position == yPosition_upward);
+}
+
+TEST_CASE("Testing Diagonal Laser downward movement functionality")
+{
+    auto xPosition = 190;
+    auto yPosition = 480;
+    auto entitySpeed = 5;
+    auto xPosition_downward = xPosition - entitySpeed;
+    auto yPosition_downward = yPosition + entitySpeed;
 	auto _laserCanon = LaserCanon(xPosition, yPosition, 1, 3);
     auto _diagonalLaser = DiagonalLaser(_laserCanon, 1);
-	
-    
-	
-	_diagonalLaser.move(Direction::UP);
-	auto [x_position, y_position] = _diagonalLaser.entityPosition();
-	CHECK(x_position == xPosition2);
-	CHECK(y_position == yPosition2);
+
+    _diagonalLaser.move(Direction::DOWN);
+    auto [x_position, y_position] = _diagonalLaser.entityPosition();
+    CHECK(x_position == xPosition_downward);
+    CHECK(y_position == yPosition_downward);
 }
